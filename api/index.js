e2be628089523e408380736d3472a710367358a2
Mazher <mazheriftikhar1@gmail.com>
2025-07-14 02:27:20 +0500

    Serverless running Locally

--- api/index.js
+++ api/index.js
@@ -4,10 +4,14 @@ const serverless = require('serverless-http');
 
 const app = express();
 
+// Set EJS and Views
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../views'));
+
+// Serve static files
 app.use(express.static(path.join(__dirname, '../public')));
 
+// Routes
 app.get('/', (req, res) => res.render('index'));
 app.get('/about', (req, res) => res.render('about'));
 app.get('/contact', (req, res) => res.render('contact'));
@@ -19,5 +23,13 @@ app.get('/portfolio-list', (req, res) => res.render('portfolio-list'));
 app.get('/portfolio-single', (req, res) => res.render('portfolio-single'));
 app.get('/single-service', (req, res) => res.render('single-service'));
 
+// Export for serverless
 module.exports = app;
 module.exports.handler = serverless(app);
+
+// 👇 This runs the server locally only
+if (require.main === module) {
+  app.listen(3000, () => {
+    console.log('✅ Server is running at http://localhost:3000');
+  });
+}
